5139c678b246a92d24d2abf82b93234cdca7bedf
thilakmani <thilakmani@kissflow.com>
2023-04-06 19:00:20 +0530

    added circle

--- src/image/tools/index.jsx
+++ src/image/tools/index.jsx
@@ -1,9 +1,11 @@
 import { Pen } from "./pen"
+import { Circle } from "./shapes/circle"
 
 export const AnnotationToolbar = () =>{
   return (
     <div>
       <Pen />
+      <Circle />
     </div>
   )
 }
